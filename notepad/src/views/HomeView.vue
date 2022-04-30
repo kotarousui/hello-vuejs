d373c2a16714e27882602c9b6462ba734ac0bd8e
kotarousui <20251436+kotarousui@users.noreply.github.com>
2022-04-30 13:08:18 +0900

    first commit

---
+++ notepad/src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <Header>My Memos</Header>
+    <ul v-for="memo in newest" :key="memo.id">
+      <li>
+        <router-link :to="{name: 'edit', params: { id: memo.id }}">
+          {{ memo.body }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+textarea {
+    width: 100%;
+    height: 10em;
+}
+button {
+    border: 1px solid #333;
+    background-color: #333;
+    color: #fff;
+    padding: 10px 20px;
+    margin-top: 10px;
+}
+</style>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Header
+  },
+  computed: { 
+    newest: function() {
+      return this.$store.state.memos.slice().reverse();
+    }
+  }
+}
+</script>
---
+++ notepad/src/views/NewView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="new">
+        <Header>New memo</Header>
+        <textarea name="memo" v-model="memoBody"></textarea>
+        <button @click="save">保存</button>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+    name: "NewView",
+    components: {
+        Header
+    },
+    data: function() {
+        return {
+            memoBody: ""
+        }
+    },
+    methods: {
+        save: function() {
+            this.$store.commit("save", {
+                body: this.memoBody
+            })
+            this.$router.push("/");
+        }
+    }
+}
+</script>
